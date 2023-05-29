5598c0d5c61ae2d271c98b62ad07b0d2eb133dbe
ouwargui <guikemon2@gmail.com>
2023-05-29 20:45:29 -0300

    feat: add word to footer accent color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer>
       <div className="flex flex-col gap-4 py-4">
         <p className="text-white text-center">
-          This website is a <span className="accent">NEVER</span> ending work in
+          This website is a <span className="accent">NEVER ENDING</span> work in
           progress.
         </p>
       </div>
