c597c297e669a6a90d73e904d5c0d85b5e9bd6bc
ouwargui <guikemon2@gmail.com>
2023-05-31 12:42:53 -0300

    fix: text color and links not opening in new tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ const posts = [
 
 export default function Home() {
   return (
-    <main className="relative px-4 flex flex-1 flex-col gap-16 py-8 md:max-w-screen-md lg:max-w-screen-lg">
+    <main className="relative px-4 flex flex-1 flex-col gap-16 py-8 md:max-w-screen-md lg:max-w-screen-lg text-white">
       <section className="flex flex-col gap-4">
         <h1 className="text-3xl md:text-5xl font-semibold text-white self-center">
           🙋🏻‍♂️ Hello, world!
@@ -50,21 +50,30 @@ export default function Home() {
             projects here. Stay tuned! 🤓
           </p>
           <div className="flex gap-4 font-bold justify-center items-center accent">
-            <span className="hover:underline">
-              <Link target="_blank" href="https://github.com/ouwargui">
-                🐙 Github
-              </Link>
-            </span>
-            <span className="hover:underline">
-              <Link target="_blank" href="https://twitter.com/eoqguih">
-                🐦 Twitter
-              </Link>
-            </span>
-            <span className="hover:underline">
-              <Link target="_blank" href="https://linkedin.com/in/guiksantos">
-                💼 LinkedIn
-              </Link>
-            </span>
+            <a
+              className="hover:underline"
+              href="https://github.com/ouwargui"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              🐙 Github
+            </a>
+            <a
+              className="hover:underline"
+              href="https://twitter.com/eoqguih"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              🐦 Twitter
+            </a>
+            <a
+              className="hover:underline"
+              href="https://linkedin.com/in/guiksantos"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              💼 LinkedIn
+            </a>
           </div>
         </div>
         <div className="w-40 md:w-[80rem] aspect-square relative rounded-full overflow-hidden">
@@ -79,7 +88,12 @@ export default function Home() {
         <h2 className="text-xl md:text-2xl font-semibold">Projects</h2>
         <div className="grid gap-4 md:grid-cols-2">
           {projects.map((project, index) => (
-            <Link href={project.href} key={index.toString()}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={project.href}
+              key={index.toString()}
+            >
               <div className="group relative hover:scale-95 overflow-hidden transition-transform rounded-xl p-[1.5px] items-center flex justify-center">
                 <div className="flex w-full h-full bg-zinc-800 flex-col gap-2 p-4 rounded-xl text-sm md:text-base">
                   <h2 className="font-semibold text-lg">{project.name}</h2>
@@ -89,7 +103,7 @@ export default function Home() {
                 </div>
                 <div className="absolute bg-white self-center -z-10 w-[calc(100%+2rem)] aspect-square rounded-full group-hover:bg-gradient-to-br group-hover:from-orange-200 group-hover:to-orange-900 group-hover:animate-[spin_1s_linear_infinite]" />
               </div>
-            </Link>
+            </a>
           ))}
         </div>
       </section>
