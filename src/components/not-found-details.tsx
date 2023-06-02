30def46bc0f54f9fb56c693340463e8e12d27756
ouwargui <guikemon2@gmail.com>
2023-06-01 21:46:19 -0300

    feat: change font size on not found page

--- src/components/not-found-details.tsx
+++ src/components/not-found-details.tsx
@@ -10,10 +10,12 @@ type Props = {
 export function NotFoundDetails(props: Props) {
   return (
     <div className="absolute flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-zinc-200 mb-4">
+      <h1 className="text-2xl md:text-4xl font-bold text-zinc-200 mb-4">
         404 - Page Not Found
       </h1>
-      <p className="text-zinc-400 mb-8">{props.description}</p>
+      <p className="text-zinc-400 mb-8 text-sm md:text-base">
+        {props.description}
+      </p>
       <Link
         href={props.href}
         className="hover:bg-white text-white hover:text-zinc-900 border border-white font-semibold py-2 px-4 rounded"
