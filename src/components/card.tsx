591c93bf000b229511d1ad0fcca59fb7b8d85644
ouwargui <guikemon2@gmail.com>
2023-07-12 19:59:24 -0300

    feat: add blurred border to card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -11,8 +11,9 @@ type Props = {
 
 export function Card({title, description, date}: Props) {
   return (
-    <div className="group relative hover:scale-95 overflow-hidden transition-transform rounded-xl p-[1.5px] items-center flex justify-center">
-      <div className="flex w-full h-full bg-zinc-800 flex-col gap-2 p-4 rounded-xl text-sm md:text-base">
+    <div className="group relative hover:scale-[.98] transition-transform rounded-xl p-[1.5px] items-center flex justify-center">
+      <div className="absolute inset-0 rounded-xl bg-orange-400 blur-sm opacity-50 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
+      <div className="relative flex w-full h-full bg-zinc-800 flex-col gap-2 p-4 rounded-xl text-sm md:text-base">
         <h2 className="font-semibold text-lg">{title}</h2>
         <span className="text-zinc-400 text-justify">{description}</span>
         {date && (
