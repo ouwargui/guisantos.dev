9bee5ce6e98cd3427780a244a5dc397ede55886b
ouwargui <guikemon2@gmail.com>
2025-04-27 13:20:26 -0300

    fix: lint error

--- src/app/(v1)/about/page.tsx
+++ src/app/(v1)/about/page.tsx
@@ -1,5 +1,5 @@
 export default function About() {
   return (
-    <main className="relative flex flex-1 flex-col gap-16 p-8 md:max-w-screen-md lg:max-w-screen-lg text-white"></main>
+    <main className="relative flex flex-1 flex-col gap-16 p-8 md:max-w-screen-md lg:max-w-screen-lg text-white" />
   );
 }
