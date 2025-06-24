998296557422db795e243ddb0a1773657f92e78a
ouwargui <guikemon2@gmail.com>
2025-06-24 19:30:06 -0300

    fix: html background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-background">
       <body>
         {children}
         <Analytics />
