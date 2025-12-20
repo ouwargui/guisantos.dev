7540cabc71fb7ad8eadb51cf10b703025f8bd468
ouwargui <guikemon2@gmail.com>
2025-12-19 22:31:00 -0300

    feat: new post

--- src/components/mdx/video.tsx
+++ src/components/mdx/video.tsx
@@ -2,7 +2,7 @@ import type {ComponentProps} from 'react';
 
 export function Video(props: ComponentProps<'video'>) {
   return (
-    <div className="self-center max-w-[222px] aspect-[222/480] overflow-clip rounded-lg">
+    <div className="self-center max-w-[222px] aspect-222/480 overflow-clip rounded-lg">
       <video autoPlay loop muted playsInline {...props} />
     </div>
   );
